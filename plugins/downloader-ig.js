42d9d0f505b255ce8abfdef1b1850007d561b4ef
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-01-15 20:47:23 +0530

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,17 +1,25 @@
-import fetch from 'node-fetch';
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(args[0])) throw m.reply(`Use example ${usedPrefix}${command} link`);
-    await m.reply(`Ｌｏａｄｉｎｇ．．．`);
-    let tes = await fetch(`https://inrl-web.onrender.com/api/insta?url=${args[0]}`);
-            const json = await tes.json();
-            const igdl = json.result;
-            await conn.sendFile(m.chat, igdl, 'error.mp4', '❤️ done here', m);
+import { instagramdl } from '@bochilteam/scraper'
+var handler = async (m, { args }) => {
+    if (!args[0]) throw 'Input URL'
+    try { 
+    	let res = await bochil.snapsave(args[0]) 
+    let media = await res[0].url
+    if (!res) throw 'Can\'t download the post'
+    m.reply('_In progress, please wait..._')
+    conn.sendMessage(m.chat, { video : { url : media }}, m) 
+    } catch {
+     try {
+     	let res2 = await instagramdl(args[0]) 
+   let media2 = res2.url
+   let cap = res2.title
+   return conn.sendFile(m.chat, media2, 'instagram.mp4', cap, m)
+  } catch {
+  conn.sendFile(m.chat, media, '','',m) } finally {
+   }
+  }
 }
-
-handler.help = ['ig'].map(v => v + ' <url>')
+handler.help = ['instagram']
 handler.tags = ['downloader']
-
 handler.command = /^(ig(dl)?|insta|instagram(dl)?)$/i
 
 export default handler
